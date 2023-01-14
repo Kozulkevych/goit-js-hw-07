d04bd453b3b4a3329555d7738c39bcc0d8267ec4
Kozulkevych Nadiia <nkozulkevych@gmail.com>
2023-01-14 08:42:05 +0200

    solving the task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-
+import { galleryItems } from "./gallery-items.js";
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryItemsMarkup);
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+
+gallery.addEventListener("click", onGalleryItemClick);
+
+function onGalleryItemClick(e) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const selectedImage = event.target.dataset.source;
+  const modal = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+  `);
+  modal.show();
+
+  gallery.addEventListener("keydown", (e) => {
+    if (event.key === "Escape") {
+      modal.close();
+    }
+  });
+}
+
+// console.log();
