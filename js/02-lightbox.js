3de053706ed00a7375bf9a0cbbd7b6db950f876a
Kozulkevych Nadiia <nkozulkevych@gmail.com>
2023-01-15 05:07:59 +0200

    змінила розмітку галереї

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -10,16 +10,9 @@ function createGalleryItemsMarkup(items) {
   return items
     .map(({ preview, original, description }) => {
       return `
-      <div class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-      </a>
-    </div>`;
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
     })
     .join("");
 }
